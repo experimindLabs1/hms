a62ad329eca5b2d29c2ac86b7125e1b608ca7947
Swapnil <swapnilsharma806@gmail.com>
2025-01-09 20:08:18 +0530

    Updated configuration for Adaptable.io

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    experimental: {
+        serverActions: true,
+    },
+    env: {
+        PORT: process.env.PORT || 3000
+    }
 }
 
 module.exports = nextConfig 
